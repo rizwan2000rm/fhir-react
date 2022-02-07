e0db1720734cc9763e308f84ca2aec151c9952e2
Rizwan Memon <rizwan2000.rm@gmail.com>
2022-02-07 15:16:06 +0530

    removed raw button support

--- src/components/containers/ResourceContainer/ResourceContainer.js
+++ src/components/containers/ResourceContainer/ResourceContainer.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import CodeBlock from '../CodeBlock';
+// import CodeBlock from '../CodeBlock';
 import './ResourceContainer.css';
 
 class ResourceContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      jsonOpen:
-        typeof this.props.jsonOpen === 'undefined'
-          ? false
-          : this.props.jsonOpen,
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     jsonOpen:
+  //       typeof this.props.jsonOpen === 'undefined'
+  //         ? false
+  //         : this.props.jsonOpen,
+  //   };
+  // }
 
   render() {
     return (
       <div className="fhir-container__ResourceContainer__card">
         <div className="fhir-container__ResourceContainer__card-body">
-          <div className="fhir-container__ResourceContainer__json-button-wrapper">
+          {/* <div className="fhir-container__ResourceContainer__json-button-wrapper">
             <button
               type="button"
               className="fhir-container__ResourceContainer__json-button"
@@ -25,9 +25,9 @@ class ResourceContainer extends React.Component {
             >
               RAW
             </button>
-          </div>
+          </div> */}
           {this.props.children}
-          <div
+          {/* <div
             className={
               this.state.jsonOpen
                 ? 'fhir-container__ResourceContainer__json--visible'
@@ -35,7 +35,7 @@ class ResourceContainer extends React.Component {
             }
           >
             <CodeBlock code={this.props.fhirResource} />
-          </div>
+          </div> */}
         </div>
       </div>
     );
